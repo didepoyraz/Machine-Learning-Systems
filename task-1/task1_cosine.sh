a1a0389cd863aa544f0014ddb4ae7a45154013a6
didepoyraz <didepyz@gmail.com>
2025-03-04 15:49:55 +0000

    added smth to the script

--- task-1/task1_cosine.sh
+++ task-1/task1_cosine.sh
@@ -4,4 +4,10 @@
 #SBATCH --output=output_task1_cosine.log
 #SBATCH --error=error_task1_cosine.log
 
+# Load Conda into the shell
+source ~/miniconda3/bin/activate  # Activate base environment
+conda activate mlsys  # Activate your environment
+
+# python json_test_file_generation.py
+# Run Python script
 python task.py
